4459d667c365d1a4514af0feaf275d34288c83bc
bluebee123 <name_unknown@web.de>
2015-10-07 12:28:57 +1100

    markers display correctly, infowindow started but not finished yet

--- js/app.js
+++ js/app.js
@@ -3,51 +3,58 @@ var initialAnimals=[
 	{
 		anName: "Southern Right Whale",
 		imgSrc: "http://www.australiananimallearningzone.com/wp-content/uploads/2012/09/Southern-Right-Whale-Pictures-300x272.jpg",
-		lat: [-38.290435],
-		lng: [142.474970 ]
+		lat: [-38.428914],
+		lng: [142.521175],
+		myMarkers: []
 	},
 	{
 		anName: "Wallaby",
 		imgSrc: "img/kang.jpg",
 		lat: [-38.698435,-38.501899],
-		lng: [143.229360,145.238695]
+		lng: [143.229360,145.238695],
+		myMarkers: []
 	},
 	{
 		anName: "Echidna",
 		imgSrc: "img/echidna.jpg",
 		lat: [-37.181397],
-		lng: [145.861132]
+		lng: [145.861132],
+		myMarkers: []
 	},
 	{
 		anName: "Little Penguin",
 		imgSrc: "img/littlePenguin.jpg",
 		lat: [-38.514876],
-		lng: [145.144377]
+		lng: [145.144377],
+		myMarkers: []
 	},
 	{
 		anName: "Possum",
 		imgSrc: "img/possum.jpg",
 		lat: [-37.930777],
-		lng: [145.111319]
+		lng: [145.111319],
+		myMarkers: []
 	},
 	{
 		anName: "Koala",
 		imgSrc: "img/koala.jpg",
 		lat: [-38.805814],
-		lng: [143.535075]
+		lng: [143.535075],
+		myMarkers: []
 	}
 
 ]
 
-//the map
+//initializes and offers methods relating to anything concerning the google map.
 var Map = function() {
-	this.markers = [];
+
 	this.initialize = function() {
-		console.log("initMap");
 	  // Create a map object and specify the DOM element for display.
 	    this.map = new google.maps.Map(document.getElementById('map'), {
 	    	center: {lat: -37.8180, lng: 143.9760},
-	    	zoom: 8
+	    	zoom: 8,
+	    	mapTypeControl: false,
+	    	draggable: false
 		});
 
 		//add searchbox
@@ -59,60 +66,112 @@ var Map = function() {
     	filterButton.index=1;
     	this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(filterButton);
 
-    	var animImage = document.getElementById('infoImage');
-    	animImage.index=1;
-    	this.map.controls[google.maps.ControlPosition.BOTTOM_LEFT].push(animImage);
+    	//on the infoPanel are elements with information to markers
+
+    	var listv = document.getElementById('listview');
+    	listv.index=1;
+    	this.map.controls[google.maps.ControlPosition.LEFT].push(listv);
+
+    /*	var infoPanel = document.getElementById('infoPanel');
+    	infoPanel.index=1;
+    	this.map.controls[google.maps.ControlPosition.LEFT].push(infoPanel);
+*/
 	}
 
-	//add some hard-coded markers
-	this.addInitialMarkers = function(data) {
-		for( var i = 0 ; i < data.lng.length; i++) {
-			this.markers.push(new google.maps.Marker({
-				position: { lat: data.lat[i], lng: data.lng[i] },
-				map: this.map,
-				title: data.anName
-			}));
-		}
+	this.getMap = function() {
+		return this.map;
 	}
 
 };
 
-//animals are theo bjects which are bound to the markers.
-var Animal = function(data) {
-	this.anName = data.anName;
-	this.imgSrc = data.imgSrc;
-	this.lngLat = data.lngLat;
-}
-
-//hardcode some animals
 
 // ko Viewmodel
-var ViewModel = function() {
+var ViewModel = function(map) {
 	var self = this;
-	self.filteredAnimal = ko.observable("Brumm");
+	self.map = map.getMap();
+	self.filteredAnimal=ko.observable("");
 	self.selectedAnimal = ko.observable();
-	self.animalList = ko.observableArray([]);
-	self.animalNames = ko.observableArray([]);
-	initialAnimals.forEach( function(animalItem) {
-		self.animalList.push( new Animal(animalItem));
-		self.animalNames.push(animalItem.anName);
-	});
-	console.log(self.animalNames());
-	$( "#filter").autocomplete({
-      source: self.animalNames()
-    });
+	self.lastAnimal = ko.observable();
 
-
-	self.updateInfo = function() {
-		var index = self.animalNames().indexOf(self.filteredAnimal());
-		if ( index > -1){
-			console.log("Selected: " + self.filteredAnimal() );
-			self.selectedAnimal(self.animalList()[index]);
-			console.log(self.selectedAnimal());
-
+//set up the info window, its content changing based on the selected animal.
+	self.infoWindow = ko.observable(new google.maps.InfoWindow());
+	self.infoContent = ko.computed(function() {
+		if( self.selectedAnimal()!==undefined) {
+			var retString = "<h4>" + self.selectedAnimal().anName + "</h4>";
+			retString +="<img id='infoImage'  src='"+ self.selectedAnimal().imgSrc +'" alt="Animal image">';
+			retString +="Wikipedia " + "placeholder";
+			return retString;
 		} else {
-			console.log("No valid animal selected.");
-			console.log(self.filteredAnimal());
+			return "nocontent";
+		}
+	})
+
+//add animals to the list
+	self.animalList = ko.observableArray([]);
+	initialAnimals.forEach( function(animalItem) {
+		//add the google maps markers
+		for(var i = 0; i < animalItem.lng.length; i++) {
+			var alat = animalItem.lat[i];
+			var alng = animalItem.lng[i];
+			var marker = new google.maps.Marker({
+ 				position: {lat: alat, lng: alng},
+             	map: self.map,
+				animation: google.maps.Animation.DROP,
+				name: animalItem.anName
+	         })
+			animalItem.myMarkers.push(marker);
+		}
+		self.animalList.push(animalItem);
+	});
+
+//the list shows only the entrys matching the content of the search box
+	self.filteredItems = ko.computed(function() {
+		var search  = self.filteredAnimal().toLowerCase();
+		return ko.utils.arrayFilter(self.animalList(), function (animal) {
+        	var show = animal.anName.toLowerCase().indexOf(search) >= 0;
+        	animal.myMarkers.forEach(function(marker){
+        		marker.setVisible(show);
+        	});
+        	return show;
+    	});
+	});
+
+
+	//set the list size to the amount of animals, or 15 at most
+	var sizeVal = self.animalList().length > 15 ? 15 : self.animalList().length;
+	$('#listview').attr('size', sizeVal);
+	// document.getElementById("listview").size = sizeVal;
+
+//on selecting an animal, the infowindow is displayed and the markers set to bouncing. the last selected animal
+//is recorded in order to unbounce their markers.
+	self.updateInfo = function() {
+		if(self.lastAnimal()) {
+			self.bounceMarkers(self.lastAnimal(),null);
+			self.toggleInfoWindow(self.lastAnimal(),false);
+		}
+		self.bounceMarkers(self.selectedAnimal(),google.maps.Animation.BOUNCE);
+		self.toggleInfoWindow(self.selectedAnimal(),true);
+		self.lastAnimal(self.selectedAnimal());
+		self.infoWindow().setContent(self.infoContent());
+	};
+
+//set the bounce animation of an animal marker on or off
+	self.bounceMarkers = function(animal, animation) {
+		animal.myMarkers.forEach( function(marker) {
+			marker.setAnimation(animation);
+		});
+	};
+
+//toggle the info window for the markers
+	self.toggleInfoWindow = function(animal,open) {
+		if(open) {
+			animal.myMarkers.forEach( function(marker) {
+				self.infoWindow().open(self.map,marker);
+			});
+		} else {
+			animal.myMarkers.forEach( function(marker) {
+				self.infoWindow().close(self.map,marker);
+			});
 		}
 	}
 }
@@ -121,8 +180,5 @@ var ViewModel = function() {
 $(window).load(function() {
   var map = new Map();
   map.initialize();
-  initialAnimals.forEach( function(animalItem) {
-  	map.addInitialMarkers(animalItem);
-  })
-  ko.applyBindings(new ViewModel());
+  ko.applyBindings(new ViewModel(map));
 });
