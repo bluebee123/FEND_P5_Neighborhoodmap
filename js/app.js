b86761b160da294c5d75497c6ed653b2bdcda798
bluebee123 <name_unknown@web.de>
2015-10-08 17:12:38 +1100

    commit before I destroy everything with google map knockout bindings.

--- js/app.js
+++ js/app.js
@@ -54,7 +54,8 @@ var Map = function() {
 	    	center: {lat: -37.8180, lng: 143.9760},
 	    	zoom: 8,
 	    	mapTypeControl: false,
-	    	draggable: false
+	    	draggable: false,
+	    	panControl: false
 		});
 
 		//add some additional elements to the map: filter search box
@@ -79,30 +80,27 @@ var Map = function() {
 
 // ko Viewmodel
 var ViewModel = function(map) {
+
+
 	var self = this;
 	self.map = map.getMap(); //save the actual google map
 	self.filteredAnimal=ko.observable("");
 	self.selectedAnimal = ko.observable();
 	self.lastAnimal = ko.observable();
 
-//set up the info window, its content changing based on the selected animal.
-	self.infoWindow = ko.observable(new google.maps.InfoWindow());
-	self.infoContent = ko.computed(function() {
-		if( self.selectedAnimal()!==undefined) {
-			var retString = "<h4>" + self.selectedAnimal().anName + "</h4>";
-			retString +="<img id='infoImage'  src='"+ self.selectedAnimal().imgSrc +'" alt="Animal image">';
-			retString +="Wikipedia " + "placeholder";
-			return retString;
-		} else {
-			return "nocontent";
-		}
-	})
+//set up the info bubble, its content changing based on the selected animal.
+	self.infoBubble = ko.observable(new InfoBubble({
+		disableAutoPan: true,
+		backgroundColor: 'transparent',
+		content: $("#infContent").html()
+	}));
 
+	self.wikiLink = ko.observable("");
+	self.wikiParagraph = ko.observable("");
 //add animals to the list
 	self.animalList = ko.observableArray([]);
 	initialAnimals.forEach( function(animalItem) {
 		//add the google maps markers
-		var tmp = ko.observable(animalItem);
 		for(var i = 0; i < animalItem.lng.length; i++) {
 			var alat = animalItem.lat[i];
 			var alng = animalItem.lng[i];
@@ -112,28 +110,34 @@ var ViewModel = function(map) {
 				animation: google.maps.Animation.DROP,
 				name: animalItem.anName
 	         })
+			marker.active = false;
 			//add click listeners to the markers, setting the current selected Animal.
 			marker.addListener('click', (function(animal) {
     			return function() {
     				self.setSelectedAnimal(animal);
+    				self.updateInfo();
     			};
-  			})(tmp()));
+  			})(animalItem));
 
-			tmp().myMarkers.push(marker);
+			animalItem.myMarkers.push(marker);
 		}
-		self.animalList().push(tmp);
+		self.animalList().push(animalItem);
 	});
 
 //the list shows only the entrys matching the content of the search box
 	self.filteredItems = ko.computed(function() {
 		var search  = self.filteredAnimal().toLowerCase();
-		return ko.utils.arrayFilter(self.animalList(), function (animal) {
-        	var show = animal().anName.toLowerCase().indexOf(search) >= 0;
-        	animal().myMarkers.forEach(function(marker){
+
+		var tmp = ko.utils.arrayFilter(self.animalList(), function (animal) {
+        	var show = animal.anName.toLowerCase().indexOf(search) >= 0;
+        	animal.myMarkers.forEach(function(marker){
         		marker.setVisible(show);
         	});
         	return show;
     	});
+
+    	console.log(tmp);
+    	return tmp;
 	});
 
 
@@ -145,19 +149,20 @@ var ViewModel = function(map) {
 //on selecting an animal, the infowindow is displayed and the markers set to bouncing. the last selected animal
 //is recorded in order to unbounce their markers.
 	self.updateInfo = function() {
+		self.wikiAjax(self.selectedAnimal().anName);
 		if(self.lastAnimal()) {
 			self.bounceMarkers(self.lastAnimal(),null);
 			self.toggleInfoWindow(self.lastAnimal(),false);
 		}
+
 		self.bounceMarkers(self.selectedAnimal(),google.maps.Animation.BOUNCE);
 		self.toggleInfoWindow(self.selectedAnimal(),true);
 		self.lastAnimal(self.selectedAnimal());
-		self.infoWindow().setContent(self.infoContent());
+
 	};
 //helper function which is called when clicking on a marker which are not bound to knockout.
 	self.setSelectedAnimal = function(animal) {
 		this.selectedAnimal(animal);
-		self.updateInfo();
 	}
 //set the bounce animation of an animal marker on or off
 	self.bounceMarkers = function(animal, animation) {
@@ -170,14 +175,65 @@ var ViewModel = function(map) {
 	self.toggleInfoWindow = function(animal,open) {
 		if(open) {
 			animal.myMarkers.forEach( function(marker) {
-				self.infoWindow().open(self.map,marker);
+				self.infoBubble().setContent($("#infContent").html());
+				self.infoBubble().open(self.map,marker);
 			});
 		} else {
 			animal.myMarkers.forEach( function(marker) {
-				self.infoWindow().close(self.map,marker);
+				self.infoBubble().close(self.map,marker);
 			});
 		}
 	}
+	self.wikiAjax = function(query) {
+		var wikiURL = 'http://en.wikipedia.org/w/api.php?action=opensearch&search='+query+
+    	'&format=json&callback=wikiCallback';
+
+    	$.ajax({
+        	url: wikiURL,
+        	dataType: "jsonp",
+        	success: function ( response ) {
+				self.wikiLink('http://en.wikipedia.org/wiki/' + response[1][0]);
+				self.wikiParagraph( response[2][0]);
+
+        	}
+   		 });
+	};
+
+
+}
+
+
+var testData = function() {
+	var aurl = "http://biocache.ala.org.au/ws/occurrences/search?";
+	//find them in the area of my map
+	aurl+="q=wallaby";
+	aurl+="&facets=names_and_lsid";
+
+	//find all occurences from the current month
+	var d = new Date();
+  var mon = d.getMonth();
+  var yr = d.getFullYear();
+	/*url+="&fq=month:";
+	var monStr = mon>9? mon : "0" + mon;
+	url+=monStr;
+	*/	//and current year
+   //	url+="&fq=year:'"+yr+"'";
+   aurl+="&pageSize=0";
+   	aurl+="&fq=year:" + yr;
+   	aurl+="&wtk=POLYGON((142.59910583496094+-37.00693943418585,146.47178649902344%20-37.00693943418585,146.47178649902344%20-38.83970761354512,142.59910583496094%20-38.83970761354512,142.59910583496094%20-37.00693943418585))";
+
+   	console.log(aurl);
+   	$.ajax({
+        url: "http://cors.io/?u="+aurl,
+        dataType: "json",
+        jsonCallback: 'callback',
+        success: function ( response ) {
+            console.log(response);
+
+        }
+    }).error(function(e) {
+       console.log("ERROR" + e);
+    });
 }
 
 //AIzaSyBvSyOJDU2J8YelkMVS4LGsuI0KVNGqu-I
@@ -185,4 +241,7 @@ $(window).load(function() {
   var map = new Map();
   map.initialize();
   ko.applyBindings(new ViewModel(map));
+  //testData();
+
+
 });
