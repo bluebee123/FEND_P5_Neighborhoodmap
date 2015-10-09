068644f72c4dac91caae72e57f69b91bbd7a797e
bluebee123 <name_unknown@web.de>
2015-10-09 15:31:54 +1100

    listeners to markers, they act accordingly

--- js/app.js
+++ js/app.js
@@ -86,7 +86,28 @@ var ViewModel = function(map) {
 	self.map = map.getMap(); //save the actual google map
 	self.filteredAnimal=ko.observable("");
 	self.selectedAnimal = ko.observable();
-	self.lastAnimal = ko.observable();
+	self.selectedAnimal.subscribe(function() {
+		//whenever the selected Animal changes, we fire the ajax
+		//Wikipedia
+		console.log("sel. animal changed to: " + self.selectedAnimal().anName);
+		self.wikiAjax(self.selectedAnimal().anName);
+		//all the markers of this animal are set to active
+		self.selectedAnimal().myMarkers.forEach(function(marker) {
+			console.log("activating: " + marker().id);
+			marker().active(true);
+		});
+		if(self.lastAnimal()===null) {
+			self.lastAnimal(self.selectedAnimal());
+
+		} else {
+			//all markers of last animal need to be deactivated
+			self.lastAnimal().myMarkers.forEach(function(marker) {
+				marker().active(false);
+			})
+			self.lastAnimal(self.selectedAnimal());
+		}
+	});
+	self.lastAnimal = ko.observable(null);
 
 //set up the info bubble, its content changing based on the selected animal.
 	self.infoBubble = ko.observable(new InfoBubble({
@@ -99,31 +120,58 @@ var ViewModel = function(map) {
 	self.wikiParagraph = ko.observable("");
 //add animals to the list
 	self.animalList = ko.observableArray([]);
+
 	initialAnimals.forEach( function(animalItem) {
 		//add the google maps markers
 		for(var i = 0; i < animalItem.lng.length; i++) {
 			var alat = animalItem.lat[i];
 			var alng = animalItem.lng[i];
-			var marker = new google.maps.Marker({
+			var marker = ko.observable(new google.maps.Marker({
  				position: {lat: alat, lng: alng},
              	map: self.map,
-				animation: google.maps.Animation.DROP,
-				name: animalItem.anName
-	         })
-			marker.active = false;
+				name: animalItem.anName,
+				animation: google.maps.Animation.DROP
+	         }));
+			marker().active = ko.observable(false);
+			marker().id = i + "-" + marker().name;
 			//add click listeners to the markers, setting the current selected Animal.
-			marker.addListener('click', (function(animal) {
+			marker().addListener('click', (function(animal) {
     			return function() {
-    				self.setSelectedAnimal(animal);
-    				self.updateInfo();
+
+    				self.selectedAnimal(animalItem);
     			};
   			})(animalItem));
 
+
 			animalItem.myMarkers.push(marker);
 		}
+		//add active subscriptions to each marker, which will toggle animation and close infoBubbles if active(false)
+		animalItem.myMarkers.forEach( function(marker) {
+			marker().active.subscribe(function() {
+				console.log("Marker active has been changed: " + marker().active() + " -  " + marker().id);
+				if(marker().active()) {
+					marker().setAnimation(google.maps.Animation.BOUNCE);
+
+				} else {
+					//no more animation
+					marker().setAnimation(null);
+					//the infobubble get closed as well
+					self.infoBubble().close(self.map,marker());
+				}
+
+			});
+		});
 		self.animalList().push(animalItem);
 	});
 
+
+
+
+
+
+	self.listSize = ko.computed(function() {
+		return self.animalList().length;
+	});
 //the list shows only the entrys matching the content of the search box
 	self.filteredItems = ko.computed(function() {
 		var search  = self.filteredAnimal().toLowerCase();
@@ -131,7 +179,7 @@ var ViewModel = function(map) {
 		var tmp = ko.utils.arrayFilter(self.animalList(), function (animal) {
         	var show = animal.anName.toLowerCase().indexOf(search) >= 0;
         	animal.myMarkers.forEach(function(marker){
-        		marker.setVisible(show);
+        		marker().setVisible(show);
         	});
         	return show;
     	});
@@ -148,42 +196,8 @@ var ViewModel = function(map) {
 
 //on selecting an animal, the infowindow is displayed and the markers set to bouncing. the last selected animal
 //is recorded in order to unbounce their markers.
-	self.updateInfo = function() {
-		self.wikiAjax(self.selectedAnimal().anName);
-		if(self.lastAnimal()) {
-			self.bounceMarkers(self.lastAnimal(),null);
-			self.toggleInfoWindow(self.lastAnimal(),false);
-		}
 
-		self.bounceMarkers(self.selectedAnimal(),google.maps.Animation.BOUNCE);
-		self.toggleInfoWindow(self.selectedAnimal(),true);
-		self.lastAnimal(self.selectedAnimal());
-
-	};
 //helper function which is called when clicking on a marker which are not bound to knockout.
-	self.setSelectedAnimal = function(animal) {
-		this.selectedAnimal(animal);
-	}
-//set the bounce animation of an animal marker on or off
-	self.bounceMarkers = function(animal, animation) {
-		animal.myMarkers.forEach( function(marker) {
-			marker.setAnimation(animation);
-		});
-	};
-
-//toggle the info window for the markers
-	self.toggleInfoWindow = function(animal,open) {
-		if(open) {
-			animal.myMarkers.forEach( function(marker) {
-				self.infoBubble().setContent($("#infContent").html());
-				self.infoBubble().open(self.map,marker);
-			});
-		} else {
-			animal.myMarkers.forEach( function(marker) {
-				self.infoBubble().close(self.map,marker);
-			});
-		}
-	}
 	self.wikiAjax = function(query) {
 		var wikiURL = 'http://en.wikipedia.org/w/api.php?action=opensearch&search='+query+
     	'&format=json&callback=wikiCallback';
@@ -194,7 +208,10 @@ var ViewModel = function(map) {
         	success: function ( response ) {
 				self.wikiLink('http://en.wikipedia.org/wiki/' + response[1][0]);
 				self.wikiParagraph( response[2][0]);
-
+				//only after success open the info bubble
+				self.infoBubble().setContent($("#infContent").html());
+				//only one infoBubble, even if more markers are present
+				self.infoBubble().open(self.map,self.selectedAnimal().myMarkers[0]());
         	}
    		 });
 	};
