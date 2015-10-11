4ae5b1220c330c72ef07f09448848ced2c0edc25
bluebee123 <name_unknown@web.de>
2015-10-11 11:52:11 +1100

    flickr api working in info bubble

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
-
+var INFOTABTITLE = "Pic and Info";
+var FLICKRTABTITLE = "Flickr Images";
 var initialAnimals=[
 	{
 		anName: "Southern Right Whale",
@@ -74,9 +75,9 @@ var Map = function() {
     	searchControlDiv.index = 1;
     	this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(searchControlDiv);
 
-
+    	//and the list view
     	var listv = document.getElementById('listview');
-    	listv.index=1;
+    	listv.index=0;
     	this.map.controls[google.maps.ControlPosition.LEFT].push(listv);
 
 
@@ -102,36 +103,43 @@ var ViewModel = function(map, overlay) {
 	self.overlay = overlay;
 	self.filteredAnimal=ko.observable("");
 	self.selectedAnimal = ko.observable();
+	self.lastAnimal =null;
 	self.selectedAnimal.subscribe(function() {
 		//whenever the selected Animal changes, we fire the ajax
 		//Wikipedia
 		self.wikiAjax(self.selectedAnimal().anName);
+		//Flickr
+
 		//all the markers of this animal are set to active
 		self.selectedAnimal().myMarkers.forEach(function(marker) {
-			marker().active(true);
+			marker.active(true);
 		});
-		if(self.lastAnimal()===null) {
-			self.lastAnimal(self.selectedAnimal());
+		if(self.lastAnimal===null) {
+			self.lastAnimal=self.selectedAnimal();
 
 		} else {
 			//all markers of last animal need to be deactivated
-			self.lastAnimal().myMarkers.forEach(function(marker) {
-				marker().active(false);
+			self.lastAnimal.myMarkers.forEach(function(marker) {
+				marker.active(false);
 			})
-			self.lastAnimal(self.selectedAnimal());
+			self.lastAnimal=self.selectedAnimal();
 		}
 	});
-	self.lastAnimal = ko.observable(null);
+
 
 //set up the info bubble, its content changing based on the selected animal.
-	self.infoBubble = ko.observable(new InfoBubble({
+	self.infoBubble = new InfoBubble({
 		disableAutoPan: true,
-		content: $("#infContent").html(),
 		borderRadius: 0,
+		arrowSize: 0
 
-	}));
+	});
+	self.infoBubble.addTab(INFOTABTITLE, $("#infContent").html());
+	self.infoBubble.addTab(FLICKRTABTITLE, $("#flickrContent").html());
 	self.wikiLink = ko.observable("");
 	self.wikiParagraph = ko.observable("");
+
+	self.flickrImages = ko.observableArray([]);
 //add animals to the list
 	self.animalList = ko.observableArray([]);
 
@@ -140,16 +148,16 @@ var ViewModel = function(map, overlay) {
 		for(var i = 0; i < animalItem.lng.length; i++) {
 			var alat = animalItem.lat[i];
 			var alng = animalItem.lng[i];
-			var marker = ko.observable(new google.maps.Marker({
+			var marker = new google.maps.Marker({
  				position: {lat: alat, lng: alng},
              	map: self.map,
 				name: animalItem.anName,
 				animation: google.maps.Animation.DROP
-	         }));
-			marker().active = ko.observable(false);
-			marker().id = i + "-" + marker().name;
+	         });
+			marker.active = ko.observable(false);
+			marker.id = i + "-" + marker.name;
 			//add click listeners to the markers, setting the current selected Animal.
-			marker().addListener('click', (function(animal) {
+			marker.addListener('click', (function(animal) {
     			return function() {
 
     				self.selectedAnimal(animalItem);
@@ -159,16 +167,16 @@ var ViewModel = function(map, overlay) {
 		}
 		//add active subscriptions to each marker, which will toggle animation and close infoBubbles if active(false)
 		animalItem.myMarkers.forEach( function(marker) {
-			marker().active.subscribe(function() {
-				console.log("Marker active has been changed: " + marker().active() + " -  " + marker().id);
-				if(marker().active()) {
-					marker().setAnimation(google.maps.Animation.BOUNCE);
+			marker.active.subscribe(function() {
+
+				if(marker.active()) {
+					marker.setAnimation(google.maps.Animation.BOUNCE);
 
 				} else {
 					//no more animation
-					marker().setAnimation(null);
+					marker.setAnimation(null);
 					//the infobubble get closed as well
-					self.infoBubble().close(self.map,marker());
+					self.infoBubble.close(self.map,marker);
 				}
 
 			});
@@ -176,29 +184,20 @@ var ViewModel = function(map, overlay) {
 		self.animalList().push(animalItem);
 	});
 
-	self.listSize = ko.computed(function() {
-		return self.animalList().length;
-	});
 //the list shows only the entrys matching the content of the search box
 	self.filteredItems = ko.computed(function() {
 		var search  = self.filteredAnimal().toLowerCase();
 
-		var tmp = ko.utils.arrayFilter(self.animalList(), function (animal) {
+		return  ko.utils.arrayFilter(self.animalList(), function (animal) {
         	var show = animal.anName.toLowerCase().indexOf(search) >= 0;
         	animal.myMarkers.forEach(function(marker){
-        		marker().setVisible(show);
+        		marker.setVisible(show);
         	});
         	return show;
     	});
 
-    	console.log(tmp);
-    	return tmp;
 	});
 
-
-	//set the list size to the amount of animals, or 15 at most
-	var sizeVal = self.animalList().length > 15 ? 15 : self.animalList().length;
-	$('#listview').attr('size', sizeVal);
 	// document.getElementById("listview").size = sizeVal;
 
 //on selecting an animal, the infowindow is displayed and the markers set to bouncing. the last selected animal
@@ -216,31 +215,73 @@ var ViewModel = function(map, overlay) {
 				self.wikiLink('http://en.wikipedia.org/wiki/' + response[1][0]);
 				self.wikiParagraph( response[2][0]);
 				//only after success open the info bubble
-				self.openInfoBubble();
+				self.flickrAjax(self.selectedAnimal().anName);
+
 
         	}
    		 });
 	};
 
+	self.flickrAjax = function(query) {
+		// key b6c9f7719ed5609ad7c941035ef290f9
+		//secret  56a751a4c0def0eeio
+		var flickrURL = 'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=b6c9f7719ed5609ad7c941035ef290f9';
+
+		flickrURL += "&content_type=1";
+		flickrURL+="&tags=" + query;
+
+		//search only for pics in the area of my map!
+		flickrURL+="&bbox=142,-39,146,-36";
+		//get the URL to the image thumbnail and original as well, and the geo information
+		flickrURL+="&extras=url_t,url_o,geo";
+		flickrURL+="&format=json";
+
+		flickrURL+="&jsoncallback=?";
+		$.getJSON( flickrURL, function( data ) {
+			var photoItem = {};
+			self.flickrImages([]);
+			var showArray = [];
+			if(data.photos.photo.length < 15) {
+				showArray = data.photos.photo;
+			} else {
+				//pick 15 random pictures from the original array;
+				for(var i = 0; i < 15; i++ ) {
+					console.log(data.photos.photo.length);
+					var index = Math.floor(Math.random() * data.photos.photo.length);
+ 					showArray.push(data.photos.photo[index]);
+ 					data.photos.photo.splice(index,1);
+				}
+			}
+       	 	showArray.forEach( function(photo) {
+       	 		photoItem.title = photo.title;
+       	 		photoItem.o_url = photo.url_o;
+       	 		photoItem.t_url = photo.url_t;
+       	 		photoItem.lon = photo.longitude;
+       	 		photoItem.lat = photo.latitude;
+       	 		self.flickrImages.push(photoItem);
+       	 	});
+         self.openInfoBubble();
+
+    	}).error(function(e) {
+    		console.log(e);
+    	});
+	}
+
 	self.openInfoBubble = function() {
 		//only one infoBubble, even if more markers are present, so only the first marker
 		var marker = self.selectedAnimal().myMarkers[0];
-		console.log("initial pos: " + marker().getPosition());
-		var pixPosition = self.overlay.getProjection().fromLatLngToDivPixel(marker().getPosition());
-
+		var pixPosition = self.overlay.getProjection().fromLatLngToDivPixel(marker.getPosition());
 		var xPix = pixPosition.x;
 		var yPix = pixPosition.y;
 		var contentWidth = $("#infContent").width();
 		var contentHeight = $("#infContent").height();
 		if(xPix > window.innerWidth-contentWidth/2) {
 			//we are too close to the right edge
-			console.log("a");
 			xPix = window.innerWidth - contentWidth/2 -20;
 		}
 		if(xPix < contentWidth/2) {
-			console.log("b");
 			//too close to the left edge
-			xPix = contentWidth/2;
+			xPix = contentWidth/2 + 20;
 		}
 
 		if(yPix < contentHeight) {
@@ -252,49 +293,18 @@ var ViewModel = function(map, overlay) {
 		var tmp = new google.maps.Point(xPix,yPix);
 
 		var lngLatPos = self.overlay.getProjection().fromDivPixelToLatLng(new google.maps.Point(xPix,yPix));
-		console.log("new pos" + lngLatPos);
-		self.infoBubble().setContent($("#infContent").html());
-		self.infoBubble().open(self.map);
+		self.infoBubble.updateTab(0,INFOTABTITLE,$("#infContent").html());
+		self.infoBubble.updateTab(1,FLICKRTABTITLE,$("#flickrContent").html());
+		self.infoBubble.open(self.map);
 		//self.infoBubble().open(self.map,self.selectedAnimal().myMarkers[0]());
-		self.infoBubble().setPosition(lngLatPos);
+		self.infoBubble.setPosition(lngLatPos);
+		self.infoBubble.setZIndex(2000);
 	};
 
 
 }
 
 
-var testData = function() {
-	var aurl = "http://biocache.ala.org.au/ws/occurrences/search?";
-	//find them in the area of my map
-	aurl+="q=wallaby";
-	aurl+="&facets=names_and_lsid";
-
-	//find all occurences from the current month
-	var d = new Date();
-  var mon = d.getMonth();
-  var yr = d.getFullYear();
-	/*url+="&fq=month:";
-	var monStr = mon>9? mon : "0" + mon;
-	url+=monStr;
-	*/	//and current year
-   //	url+="&fq=year:'"+yr+"'";
-   aurl+="&pageSize=0";
-   	aurl+="&fq=year:" + yr;
-   	aurl+="&wtk=POLYGON((142.59910583496094+-37.00693943418585,146.47178649902344%20-37.00693943418585,146.47178649902344%20-38.83970761354512,142.59910583496094%20-38.83970761354512,142.59910583496094%20-37.00693943418585))";
-
-   	console.log(aurl);
-   	$.ajax({
-        url: "http://cors.io/?u="+aurl,
-        dataType: "json",
-        jsonCallback: 'callback',
-        success: function ( response ) {
-            console.log(response);
-
-        }
-    }).error(function(e) {
-       console.log("ERROR" + e);
-    });
-}
 
 //AIzaSyBvSyOJDU2J8YelkMVS4LGsuI0KVNGqu-I
 $(window).load(function() {
